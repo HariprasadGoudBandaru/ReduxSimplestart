62217eb2a15f0ec638c3e45488e05bb382edd4de
Hariprasad Bandaru <orclhariprasad@gmail.com>
2017-10-25 03:48:49 +0530

    Add files via upload

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+    return[
+        {title:'Ready To Fly',pages:101},
+        {title:'Eat The Frog',pages:205},
+        {title:'who will cry when your die',pages:412},
+        {title:'Love Story',pages:255},
+        {title:'The Last Song',pages:301}
+    ]
+}
